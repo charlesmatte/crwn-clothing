17be8ba31c18df8416789c30f688d66c56af5229
Charles Matte <Matte.Charles@gmail.com>
2022-08-10 20:21:41 -0400

    fixed styling conflicts

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,11 +1,11 @@
-import CategoryItem from "../category-item/category-item.jsx";
+import DirectoryItem from "../directory-item/directory-item.jsx";
 import "./directory.styles.scss";
 
 const Directory = ({ categories }) => {
   return (
     <div className="directory-container">
       {categories.map((category) => (
-        <CategoryItem key={category.id} category={category} />
+        <DirectoryItem key={category.id} category={category} />
       ))}
     </div>
   );
