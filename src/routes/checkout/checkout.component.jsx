ac218d8de2a481251fafc82d0e52956dae29f0ee
Charles Matte <Matte.Charles@gmail.com>
2023-01-22 18:55:01 -0500

    added stripe payment

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -5,6 +5,7 @@ import {
 } from "../../store/cart/cart.selector";
 
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import PaymentForm from "../../components/payment-form/payment-form.component";
 import "./checkout.styles.scss";
 
 const Checkout = () => {
@@ -33,6 +34,7 @@ const Checkout = () => {
         return <CheckoutItem key={cartItem.id} cartItem={cartItem} />;
       })}
       <span className="total">Total: ${cartTotal}</span>
+      <PaymentForm />
     </div>
   );
 };
